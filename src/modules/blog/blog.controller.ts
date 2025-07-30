b2978d9ab09b0dcfa010ae94a218f34123000441
Fadilah Aryadiputra <fadilaharyadiputra01@gmail.com>
2025-07-30 15:20:46 +0700

    feat: add blog-detail

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -1,7 +1,7 @@
-import { plainToInstance } from "class-transformer";
-import { Request, Response } from "express";
-import { PaginationQueryParams } from "../pagination/dto/pagination.dto";
-import { BlogService } from "./blog.service";
+import { plainToInstance } from 'class-transformer';
+import { Request, Response } from 'express';
+import { PaginationQueryParams } from '../pagination/dto/pagination.dto';
+import { BlogService } from './blog.service';
 
 export class BlogController {
   private blogService: BlogService;
@@ -12,7 +12,13 @@ export class BlogController {
 
   getBlogs = async (req: Request, res: Response) => {
     const query = plainToInstance(PaginationQueryParams, req.query);
-    const result = await this.blogService.getBlogs(query)
+    const result = await this.blogService.getBlogs(query);
+    res.status(200).send(result);
+  };
+
+  getBlogBySlug = async (req: Request, res: Response) => {
+    const slug = req.params.slug;
+    const result = await this.blogService.getBlogBySlug(slug);
     res.status(200).send(result);
   };
 }
